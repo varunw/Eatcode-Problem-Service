7fd1ae3989f472c406c0da2d4f0d3a77d39fa9d7
varun waykar <varunwaykar@varuns-MacBook-Air.local>
2024-11-20 21:31:10 +0530

    Feat: Added routing structure for versioned api for problems

---
+++ src/controllers/problem.controller.js
@@ -0,0 +1,32 @@
+function pingProblemController(req,res){
+    return res.json({message:"Ping Problem Controller"});
+}
+
+function addProblem(req,res){
+
+}
+
+function getProblem(req,res){
+
+}
+
+function getProblems(req,res){
+
+}
+
+function deleteProblem(req,res){
+
+}
+
+function updateProblem(req,res){
+
+}
+
+module.exports = {
+    addProblem,
+    getProblem,
+    getProblems,
+    deleteProblem,
+    updateProblem,
+    pingProblemController
+}
--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const bodyParser = require("body-parser");
 const { PORT } = require('./config/server.config') ;
+const apiRouter = require('./routes');
 
 
 const app = express();
@@ -9,6 +10,8 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended:true}));
 app.use(bodyParser.text());
 
+
+app.use('/api',apiRouter);
 
 app.listen(PORT,()=>{
     console.log(`Server started on port ${PORT}`)
