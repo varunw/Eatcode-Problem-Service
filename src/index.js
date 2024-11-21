e63d7db571fe137ac91355ceb74d2f464b64a31d
varun waykar <varunwaykar@varuns-MacBook-Air.local>
2024-11-21 23:35:01 +0530

    Feat: Added mongoose db config

--- src/index.js
+++ src/index.js
@@ -3,6 +3,8 @@ const bodyParser = require("body-parser");
 const { PORT } = require('./config/server.config') ;
 const apiRouter = require('./routes');
 const errorHandler = require('./utils/errorHandler');
+const connectToDb = require('./config/db.config');
+// const mongoose = require("mongoose");
 
 
 const app = express();
@@ -17,6 +19,9 @@ app.use('/api',apiRouter);
 // Last middleware if any error comes
 app.use(errorHandler);
 
-app.listen(PORT,()=>{
-    console.log(`Server started on port ${PORT}`)
+app.listen(PORT,async ()=>{
+    console.log(`Server started on port ${PORT}`);
+    await connectToDb();
+    console.log("Successfully connnected to db");
+
 })
