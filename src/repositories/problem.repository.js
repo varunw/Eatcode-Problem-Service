9cfacc071e5dc7b0f65c7993ba3215227bbafeba
varun waykar <varunwaykar@varuns-MacBook-Air.local>
2024-11-25 00:01:10 +0530

    Fix: Delete problem bug fix

--- src/repositories/problem.repository.js
+++ src/repositories/problem.repository.js
@@ -1,3 +1,4 @@
+const logger = require("../config/logger.config");
 const NotFound = require("../errors/notfound.error");
 const { Problem } = require("../models");
 
@@ -42,9 +43,10 @@ class ProblemRepository{
 
     async deleteProblem(id){
         try {
-            const problemTobedeleted = await Problem.findById(id);
-            const problem = await Problem.deleteOne(problemTobedeleted);
+            // const problemTobedeleted = await Problem.findById(id);
+            const problem = await Problem.findByIdAndDelete(id);
             if(!problem){
+                logger.error(`Problem with id ${id} not found in db`)
                 throw new NotFound("Problem",id);
             }
             return problem;
